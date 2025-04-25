1ece8ca16f63f272d4cba289e3ac10159a4050d9
thomasmeston <thomasmeston@users.noreply.github.com>
2025-04-25 04:21:03 +0000

    deploy: 4021dec48bd03a962303ae5723e58082af877f68

--- src/assets.js
+++ src/assets.js
@@ -1,11 +1,8 @@
-// Import textures
-import woodTexture from './assets/images/wood_texture.png';
-import notebookPaper from './assets/images/notebook_texture.png';
-
-// Debug log to verify the imports
-console.log('Importing textures:', { woodTexture, notebookPaper });
-
+// Define texture paths
 export const images = {
-    woodTexture,
-    notebookPaper
-}; 
+    woodTexture: 'src/assets/images/wood_texture.png',
+    notebookPaper: 'src/assets/images/notebook_texture.png'
+};
+
+// Debug log to verify the paths
+console.log('Texture paths:', images); 
